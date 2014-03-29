3c2dcbf3f7bd774fc33d500641c3cade5a736e9d
Javier Cicchelli <javier@rock-n-co.de>
2014-03-29 20:17:00 +0100

    Created and defined the constants on RCOptionKeys.

---
+++ Marvelous/RCOptionKeys.h
@@ -0,0 +1,47 @@
+//
+//  RCOptionKeys.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+extern NSString * const RCOptionKeyIdentifier;
+extern NSString * const RCOptionKeyFirstName;
+extern NSString * const RCOptionKeyMiddleName;
+extern NSString * const RCOptionKeyLastName;
+extern NSString * const RCOptionKeySuffix;
+extern NSString * const RCOptionKeyName;
+extern NSString * const RCOptionKeyNameStartsWith;
+extern NSString * const RCOptionKeyFirstNameStartsWith;
+extern NSString * const RCOptionKeyMiddleNameStartsWith;
+extern NSString * const RCOptionKeyLastNameStartsWith;
+extern NSString * const RCOptionKeyTitle;
+extern NSString * const RCOptionKeyTitleStartsWith;
+extern NSString * const RCOptionKeyFormat;
+extern NSString * const RCOptionKeyFormatType;
+extern NSString * const RCOptionKeyNoVariants;
+extern NSString * const RCOptionKeyStartYear;
+extern NSString * const RCOptionKeyDateDescriptor;
+extern NSString * const RCOptionKeyDateRange;
+extern NSString * const RCOptionKeyDiamondCode;
+extern NSString * const RCOptionKeyDigitalIdentifier;
+extern NSString * const RCOptionKeyUPC;
+extern NSString * const RCOptionKeyISBN;
+extern NSString * const RCOptionKeyEAN;
+extern NSString * const RCOptionKeyISSN;
+extern NSString * const RCOptionKeyHasDigitalIssue;
+extern NSString * const RCOptionKeyModifiedSince;
+extern NSString * const RCOptionKeyCreators;
+extern NSString * const RCOptionKeyCharacters;
+extern NSString * const RCOptionKeyComics;
+extern NSString * const RCOptionKeySeries;
+extern NSString * const RCOptionKeyEvents;
+extern NSString * const RCOptionKeyStories;
+extern NSString * const RCOptionKeySharedAppearances;
+extern NSString * const RCOptionKeyCollaborators;
+extern NSString * const RCOptionKeySeriesType;
+extern NSString * const RCOptionKeyContains;
+extern NSString * const RCOptionKeyOrderBy;
+extern NSString * const RCOptionKeyLimit;
+extern NSString * const RCOptionKeyOffset;
