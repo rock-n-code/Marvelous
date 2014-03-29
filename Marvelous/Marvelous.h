dc79722d36350ba0afb4410fae121b917ab96703
Javier Cicchelli <javier@rock-n-co.de>
2014-03-29 18:12:22 +0100

    Initial Commit

---
+++ Marvelous/Marvelous.h
@@ -0,0 +1,13 @@
+//
+//  Marvelous.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Marvelous : NSObject
+
+@end
---
+++ Marvelous/Marvelous.m
@@ -0,0 +1,13 @@
+//
+//  Marvelous.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "Marvelous.h"
+
+@implementation Marvelous
+
+@end
