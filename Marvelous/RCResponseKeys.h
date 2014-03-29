adafdcb766c0415815189f556a5af5ba4954c0db
Javier Cicchelli <javier@rock-n-co.de>
2014-03-29 22:12:29 +0100

    Created and defined the constants on RCResponseKeys.

---
+++ Marvelous/RCResponseKeys.h
@@ -0,0 +1,76 @@
+//
+//  RCResponseKeys.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+extern NSString * const RCResponseKeyCode;
+extern NSString * const RCResponseKeyStatus;
+extern NSString * const RCResponseKeyCopyright;
+extern NSString * const RCResponseKeyAttributionText;
+extern NSString * const RCResponseKeyAttributionHTML;
+extern NSString * const RCResponseKeyData;
+extern NSString * const RCResponseKeyETag;
+extern NSString * const RCResponseKeyOffset;
+extern NSString * const RCResponseKeyLimit;
+extern NSString * const RCResponseKeyTotal;
+extern NSString * const RCResponseKeyCount;
+extern NSString * const RCResponseKeyResults;
+extern NSString * const RCResponseKeyIdentifier;
+extern NSString * const RCResponseKeyDigitalIdentifier;
+extern NSString * const RCResponseKeyFirstName;
+extern NSString * const RCResponseKeyMiddleName;
+extern NSString * const RCResponseKeyLastName;
+extern NSString * const RCResponseKeySuffix;
+extern NSString * const RCResponseKeyName;
+extern NSString * const RCResponseKeyFullName;
+extern NSString * const RCResponseKeyTitle;
+extern NSString * const RCResponseKeyIssueNumber;
+extern NSString * const RCResponseKeyDescription;
+extern NSString * const RCResponseKeyVariantDescription;
+extern NSString * const RCResponseKeyStart;
+extern NSString * const RCResponseKeyEnd;
+extern NSString * const RCResponseKeyModified;
+extern NSString * const RCResponseKeyISBN;
+extern NSString * const RCResponseKeyUPC;
+extern NSString * const RCResponseKeyDiamondCode;
+extern NSString * const RCResponseKeyEAN;
+extern NSString * const RCResponseKeyISSN;
+extern NSString * const RCResponseKeyFormat;
+extern NSString * const RCResponseKeyPageCount;
+extern NSString * const RCResponseKeyTextObjects;
+extern NSString * const RCResponseKeyLanguage;
+extern NSString * const RCResponseKeyText;
+extern NSString * const RCResponseKeyResourceURI;
+extern NSString * const RCResponseKeyURLs;
+extern NSString * const RCResponseKeyStartYear;
+extern NSString * const RCResponseKeyEndYear;
+extern NSString * const RCResponseKeyRating;
+extern NSString * const RCResponseKeyThumbnail;
+extern NSString * const RCResponseKeyImages;
+extern NSString * const RCResponseKeyComics;
+extern NSString * const RCResponseKeyStories;
+extern NSString * const RCResponseKeyEvents;
+extern NSString * const RCResponseKeySeries;
+extern NSString * const RCResponseKeyVariants;
+extern NSString * const RCResponseKeyCollections;
+extern NSString * const RCResponseKeyCollectedIssues;
+extern NSString * const RCResponseKeyDates;
+extern NSString * const RCResponseKeyPrices;
+extern NSString * const RCResponseKeyCreators;
+extern NSString * const RCResponseKeyCharacters;
+extern NSString * const RCResponseKeyOriginalIssue;
+extern NSString * const RCResponseKeyPrevious;
+extern NSString * const RCResponseKeyNext;
+extern NSString * const RCResponseKeyType;
+extern NSString * const RCResponseKeyURL;
+extern NSString * const RCResponseKeyPath;
+extern NSString * const RCResponseKeyExtension;
+extern NSString * const RCResponseKeyPrice;
+extern NSString * const RCResponseKeyRole;
+extern NSString * const RCResponseKeyAvailable;
+extern NSString * const RCResponseKeyReturned;
+extern NSString * const RCResponseKeyCollectionURI;
+extern NSString * const RCResponseKeyItems;
---
+++ Marvelous/RCResponseKeys.m
@@ -0,0 +1,78 @@
+//
+//  RCResponseKeys.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCResponseKeys.h"
+
+NSString * const RCResponseKeyCode = @"code";
+NSString * const RCResponseKeyStatus = @"status";
+NSString * const RCResponseKeyCopyright = @"copyright";
+NSString * const RCResponseKeyAttributionText = @"attributionText";
+NSString * const RCResponseKeyAttributionHTML = @"attributionHTML";
+NSString * const RCResponseKeyData = @"data";
+NSString * const RCResponseKeyETag = @"etag";
+NSString * const RCResponseKeyOffset = @"offset";
+NSString * const RCResponseKeyLimit = @"limit";
+NSString * const RCResponseKeyTotal = @"total";
+NSString * const RCResponseKeyCount = @"count";
+NSString * const RCResponseKeyResults = @"results";
+NSString * const RCResponseKeyIdentifier = @"id";
+NSString * const RCResponseKeyDigitalIdentifier = @"digitalId";
+NSString * const RCResponseKeyFirstName = @"firstName";
+NSString * const RCResponseKeyMiddleName = @"middleName";
+NSString * const RCResponseKeyLastName = @"lastName";
+NSString * const RCResponseKeySuffix = @"suffix";
+NSString * const RCResponseKeyName = @"name";
+NSString * const RCResponseKeyFullName = @"fullName";
+NSString * const RCResponseKeyTitle = @"title";
+NSString * const RCResponseKeyIssueNumber = @"issueNumber";
+NSString * const RCResponseKeyDescription = @"description";
+NSString * const RCResponseKeyVariantDescription = @"variantDescription";
+NSString * const RCResponseKeyStart = @"start";
+NSString * const RCResponseKeyEnd = @"end";
+NSString * const RCResponseKeyModified = @"modified";
+NSString * const RCResponseKeyISBN = @"isbn";
+NSString * const RCResponseKeyUPC = @"upc";
+NSString * const RCResponseKeyDiamondCode = @"diamondCode";
+NSString * const RCResponseKeyEAN = @"ean";
+NSString * const RCResponseKeyISSN = @"issn";
+NSString * const RCResponseKeyFormat = @"format";
+NSString * const RCResponseKeyPageCount = @"pageCount";
+NSString * const RCResponseKeyTextObjects = @"textObjects";
+NSString * const RCResponseKeyLanguage = @"language";
+NSString * const RCResponseKeyText = @"text";
+NSString * const RCResponseKeyResourceURI = @"resourceURI";
+NSString * const RCResponseKeyURLs = @"urls";
+NSString * const RCResponseKeyStartYear = @"startYear";
+NSString * const RCResponseKeyEndYear = @"endYear";
+NSString * const RCResponseKeyRating = @"rating";
+NSString * const RCResponseKeyThumbnail = @"thumbnail";
+NSString * const RCResponseKeyImages = @"images";
+NSString * const RCResponseKeyComics = @"comics";
+NSString * const RCResponseKeyStories = @"stories";
+NSString * const RCResponseKeyEvents = @"events";
+NSString * const RCResponseKeySeries = @"series";
+NSString * const RCResponseKeyVariants = @"variants";
+NSString * const RCResponseKeyCollections = @"collections";
+NSString * const RCResponseKeyCollectedIssues = @"collectedIssues";
+NSString * const RCResponseKeyDates = @"dates";
+NSString * const RCResponseKeyPrices = @"prices";
+NSString * const RCResponseKeyCreators = @"creators";
+NSString * const RCResponseKeyCharacters = @"characters";
+NSString * const RCResponseKeyOriginalIssue = @"originalissue";
+NSString * const RCResponseKeyPrevious = @"previous";
+NSString * const RCResponseKeyNext = @"next";
+NSString * const RCResponseKeyType = @"type";
+NSString * const RCResponseKeyURL = @"url";
+NSString * const RCResponseKeyPath = @"path";
+NSString * const RCResponseKeyExtension = @"extension";
+NSString * const RCResponseKeyPrice = @"price";
+NSString * const RCResponseKeyRole = @"role";
+NSString * const RCResponseKeyAvailable = @"available";
+NSString * const RCResponseKeyReturned = @"returned";
+NSString * const RCResponseKeyCollectionURI = @"collectionURI";
+NSString * const RCResponseKeyItems = @"items";
