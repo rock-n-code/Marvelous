2337f65c8068fce0ebfec86ed1ed96c1251770d7
Javier Cicchelli <javier@rock-n-co.de>
2014-03-29 23:11:36 +0100

    Created the RCAPIOperation.

---
+++ Marvelous/RCAPIOperation.h
@@ -0,0 +1,13 @@
+//
+//  RCAPIOperation.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCOperation.h"
+
+@interface RCAPIOperation : RCOperation
+
+@end
---
+++ MarvelousTests/RCAPIOperationTests.m
@@ -0,0 +1,38 @@
+//
+//  RCAPIOperationTests.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RCAPIOperation.h"
+
+@interface RCAPIOperationTests : XCTestCase
+
+@end
+
+@implementation RCAPIOperationTests
+
+- (void)setUp
+{
+    [super setUp];
+
+	// Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+
+	[super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
