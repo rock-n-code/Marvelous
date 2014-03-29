a03576243817d7f883af6ba60f912dff997d353f
Javier Cicchelli <javier@rock-n-co.de>
2014-03-29 20:40:23 +0100

    Created and defined the enumerations on RCStatusCodes.

---
+++ Marvelous/RCStatusCodes.h
@@ -0,0 +1,14 @@
+//
+//  RCStatusCodes.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+typedef NS_ENUM(NSInteger, RCStatusCodes) {
+	RCStatusCodeOK = 200,
+	RCStatusCodeNotFound = 404,
+	RCStatusCodeParameterError = 409
+};
+
