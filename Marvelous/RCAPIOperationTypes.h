2932f216d83d9d3d7179a483884718255b86f6b3
Javier Cicchelli <javier@rock-n-co.de>
2014-03-29 23:16:41 +0100

    Created and defined the enumerations on RCAPIOperationTypes.

---
+++ Marvelous/RCAPIOperationTypes.h
@@ -0,0 +1,17 @@
+//
+//  RCAPIOperationTypes.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+typedef NS_ENUM(NSInteger, RCAPIOperationTypes) {
+	RCAPIOperationTypeUndefined = 0,
+	RCAPIOperationTypeCharacter,
+	RCAPIOperationTypeComics,
+	RCAPIOperationTypeCreators,
+	RCAPIOperationTypeEvents,
+	RCAPIOperationTypeSeries,
+	RCAPIOperationTypeStories
+};
