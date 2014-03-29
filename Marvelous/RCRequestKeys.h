c36ad8711e3568ee7262aa2440c07bb8c0b3319d
Javier Cicchelli <javier@rock-n-co.de>
2014-03-29 20:47:42 +0100

    Renamed the RCOptionKeys to RCRequestKeys.

---
+++ Marvelous/RCRequestKeys.h
@@ -0,0 +1,47 @@
+//
+//  RCRequestKeys.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+extern NSString * const RCRequestKeyIdentifier;
+extern NSString * const RCRequestKeyFirstName;
+extern NSString * const RCRequestKeyMiddleName;
+extern NSString * const RCRequestKeyLastName;
+extern NSString * const RCRequestKeySuffix;
+extern NSString * const RCRequestKeyName;
+extern NSString * const RCRequestKeyNameStartsWith;
+extern NSString * const RCRequestKeyFirstNameStartsWith;
+extern NSString * const RCRequestKeyMiddleNameStartsWith;
+extern NSString * const RCRequestKeyLastNameStartsWith;
+extern NSString * const RCRequestKeyTitle;
+extern NSString * const RCRequestKeyTitleStartsWith;
+extern NSString * const RCRequestKeyFormat;
+extern NSString * const RCRequestKeyFormatType;
+extern NSString * const RCRequestKeyNoVariants;
+extern NSString * const RCRequestKeyStartYear;
+extern NSString * const RCRequestKeyDateDescriptor;
+extern NSString * const RCRequestKeyDateRange;
+extern NSString * const RCRequestKeyDiamondCode;
+extern NSString * const RCRequestKeyDigitalIdentifier;
+extern NSString * const RCRequestKeyUPC;
+extern NSString * const RCRequestKeyISBN;
+extern NSString * const RCRequestKeyEAN;
+extern NSString * const RCRequestKeyISSN;
+extern NSString * const RCRequestKeyHasDigitalIssue;
+extern NSString * const RCRequestKeyModifiedSince;
+extern NSString * const RCRequestKeyCreators;
+extern NSString * const RCRequestKeyCharacters;
+extern NSString * const RCRequestKeyComics;
+extern NSString * const RCRequestKeySeries;
+extern NSString * const RCRequestKeyEvents;
+extern NSString * const RCRequestKeyStories;
+extern NSString * const RCRequestKeySharedAppearances;
+extern NSString * const RCRequestKeyCollaborators;
+extern NSString * const RCRequestKeySeriesType;
+extern NSString * const RCRequestKeyContains;
+extern NSString * const RCRequestKeyOrderBy;
+extern NSString * const RCRequestKeyLimit;
+extern NSString * const RCRequestKeyOffset;
---
+++ Marvelous/RCRequestKeys.m
@@ -0,0 +1,49 @@
+//
+//  RCRequestKeys.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 29/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCRequestKeys.h"
+
+NSString * const RCRequestKeyIdentifier = @"identifier";
+NSString * const RCRequestKeyFirstName = @"firstName";
+NSString * const RCRequestKeyMiddleName = @"middleName";
+NSString * const RCRequestKeyLastName = @"lastName";
+NSString * const RCRequestKeySuffix = @"suffix";
+NSString * const RCRequestKeyName = @"name";
+NSString * const RCRequestKeyNameStartsWith = @"nameStartsWith";
+NSString * const RCRequestKeyFirstNameStartsWith = @"firstNameStartsWith";
+NSString * const RCRequestKeyMiddleNameStartsWith = @"middleNameStartsWith";
+NSString * const RCRequestKeyLastNameStartsWith = @"lastNameStartsWith";
+NSString * const RCRequestKeyTitle = @"title";
+NSString * const RCRequestKeyTitleStartsWith = @"titleStartsWith";
+NSString * const RCRequestKeyFormat = @"format";
+NSString * const RCRequestKeyFormatType = @"formatType";
+NSString * const RCRequestKeyNoVariants = @"noVariants";
+NSString * const RCRequestKeyStartYear = @"startYear";
+NSString * const RCRequestKeyDateDescriptor = @"dateDescriptor";
+NSString * const RCRequestKeyDateRange = @"dateRange";
+NSString * const RCRequestKeyDiamondCode = @"damindCode";
+NSString * const RCRequestKeyDigitalIdentifier = @"digitalId";
+NSString * const RCRequestKeyUPC = @"upc";
+NSString * const RCRequestKeyISBN = @"isbn";
+NSString * const RCRequestKeyEAN = @"ean";
+NSString * const RCRequestKeyISSN = @"issn";
+NSString * const RCRequestKeyHasDigitalIssue = @"hasDigitalIssue";
+NSString * const RCRequestKeyModifiedSince = @"modifiedSince";
+NSString * const RCRequestKeyCreators = @"creators";
+NSString * const RCRequestKeyCharacters = @"characters";
+NSString * const RCRequestKeyComics = @"comics";
+NSString * const RCRequestKeySeries = @"series";
+NSString * const RCRequestKeyEvents = @"events";
+NSString * const RCRequestKeyStories = @"stories";
+NSString * const RCRequestKeySharedAppearances = @"sharedAppearances";
+NSString * const RCRequestKeyCollaborators = @"collaborators";
+NSString * const RCRequestKeySeriesType = @"seriesType";
+NSString * const RCRequestKeyContains = @"contains";
+NSString * const RCRequestKeyOrderBy = @"orderBy";
+NSString * const RCRequestKeyLimit = @"limit";
+NSString * const RCRequestKeyOffset = @"offset";
