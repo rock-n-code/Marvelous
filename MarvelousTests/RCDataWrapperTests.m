50dd2eae6938507d7be8ff84db7e96672a0789a1
Javier Cicchelli <javier@rock-n-co.de>
2014-03-31 11:19:30 +0200

    Created the RCDataWrapper.

---
+++ MarvelousTests/RCDataWrapperTests.m
@@ -0,0 +1,34 @@
+//
+//  RCDataWrapperTests.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 31/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RCDataWrapper.h"
+
+@interface RCDataWrapperTests : XCTestCase
+
+@end
+
+@implementation RCDataWrapperTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
