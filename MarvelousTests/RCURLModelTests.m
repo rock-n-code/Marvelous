06c220529b83c0b971427ca249af2c7b93ddaa7d
Javier Cicchelli <javier@rock-n-co.de>
2014-03-31 12:19:16 +0200

    Created the RCURLModel.

---
+++ MarvelousTests/RCURLModelTests.m
@@ -0,0 +1,32 @@
+//
+//  RCURLModelTests.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 31/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RCURLModelTests : XCTestCase
+
+@end
+
+@implementation RCURLModelTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
