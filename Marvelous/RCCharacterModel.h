272efd54e4d9d64f91c17e601229fd6e949d3f80
Javier Cicchelli <javier@rock-n-co.de>
2014-03-31 11:48:15 +0200

    Defined the properties for RCCharacterModel.

--- Marvelous/RCCharacterModel.h
+++ Marvelous/RCCharacterModel.h
@@ -6,6 +6,20 @@
 //  Copyright (c) 2014 Rock & Code. All rights reserved.
 //
 
+@class RCListModel;
+
 @interface RCCharacterModel : NSObject
+
+@property (nonatomic, strong) NSNumber *identifier;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *bio;
+@property (nonatomic, strong) NSDate *lastModified;
+@property (nonatomic, strong) NSURL *resourceURI;
+@property (nonatomic, strong) NSArray *urls;
+@property (nonatomic, strong) NSURL *thumbnail;
+@property (nonatomic, strong) RCListModel *comics;
+@property (nonatomic, strong) RCListModel *stories;
+@property (nonatomic, strong) RCListModel *events;
+@property (nonatomic, strong) RCListModel *series;
 
 @end
--- MarvelousTests/RCCharacterModelTests.m
+++ MarvelousTests/RCCharacterModelTests.m
@@ -26,9 +26,21 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testInit
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+	RCCharacterModel *character = [[RCCharacterModel alloc] init];
+
+	XCTAssertNil(character.identifier, @"\"%s\" is expecting the property 'identifier' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(character.name, @"\"%s\" is expecting the property 'name' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(character.bio, @"\"%s\" is expecting the property 'bio' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(character.lastModified, @"\"%s\" is expecting the property 'lastModified' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(character.resourceURI, @"\"%s\" is expecting the property 'resourceURI' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(character.urls, @"\"%s\" is expecting the property 'urls' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(character.thumbnail, @"\"%s\" is expecting the property 'thumbnail' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(character.comics, @"\"%s\" is expecting the property 'comics' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(character.stories, @"\"%s\" is expecting the property 'stories' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(character.events, @"\"%s\" is expecting the property 'events' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(character.series, @"\"%s\" is expecting the property 'series' to be NULL.", __PRETTY_FUNCTION__);
 }
 
 @end
