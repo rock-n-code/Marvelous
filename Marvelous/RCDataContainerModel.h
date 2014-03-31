35a8bec91d5a03f41140f18af7961c9a3cf21492
Javier Cicchelli <javier@rock-n-co.de>
2014-03-31 11:33:17 +0200

    Created the RCDataContainerModel.

---
+++ Marvelous/RCDataContainerModel.h
@@ -0,0 +1,13 @@
+//
+//  RCDataContainerModel.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 31/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCDataContainerModel : NSObject
+
+@end
---
+++ MarvelousTests/RCDataContainerModelTests.m
@@ -0,0 +1,34 @@
+//
+//  RCDataContainerModelTests.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 31/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RCDataContainerModel.h"
+
+@interface RCDataContainerModelTests : XCTestCase
+
+@end
+
+@implementation RCDataContainerModelTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
