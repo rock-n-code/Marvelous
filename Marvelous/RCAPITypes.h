65d52c3252c67f74f0cea847ff8b271bd76e026c
Javier Cicchelli <javier@rock-n-co.de>
2014-03-31 13:33:51 +0200

    Created and defined the enumerations on RCAPITypes.

---
+++ Marvelous/RCAPITypes.h
@@ -0,0 +1,17 @@
+//
+//  RCAPITypes.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 31/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+typedef NS_ENUM(NSInteger, RCAPITypes) {
+	RCAPITypeUndefined = 0,
+	RCAPITypeCharacters,
+	RCAPITypeComics,
+	RCAPITypeCreators,
+	RCAPITypeEvents,
+	RCAPITypeSeries,
+	RCAPITypeStories
+};
