17c43c374f6d995a90a9e7bb3d7d495e1e6fd1fd
Javier Cicchelli <javier@rock-n-co.de>
2014-03-31 18:37:24 +0200

    Created the RCObject.

---
+++ Marvelous/RCObject.h
@@ -0,0 +1,13 @@
+//
+//  RCObject.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 31/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCObject : NSObject
+
+@end
---
+++ Marvelous/RCObject.m
@@ -0,0 +1,13 @@
+//
+//  RCObject.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 31/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCObject.h"
+
+@implementation RCObject
+
+@end
