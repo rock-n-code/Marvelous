072f2e7f5340bd999f09966b8bccd6831253171a
Javier Cicchelli <javier@rock-n-co.de>
2014-03-31 16:06:01 +0200

    Modified some enumeration values and also removed some others from RCOperationErrorCodes.

--- Marvelous/RCOperationErrorCodes.h
+++ Marvelous/RCOperationErrorCodes.h
@@ -7,14 +7,8 @@
 //
 
 typedef NS_ENUM(NSInteger, RCOperationErrorCodes) {
-	RCOperationErrorCodeOperationCancelled = -1,
-	RCOperationErrorCodeTypeUndefined = -1001,
-	RCOperationErrorCodePublicKeyIsNull = -1002,
-	RCOperationErrorCodeIdentifierIsNull = -1003,
-	RCOperationErrorCodeJSONStatus = -1004,
-	RCOperationErrorCodeInvalidParameter = -1005,
-	RCOperationErrorCodeAccessForbidden = -1006,
-	RCOperationErrorCodeDataNotFound = -1007,
-	RCOperationErrorCodeMethodNotAllowed = -1008,
-	RCOperationErrorCodeParameterError = -1009,
+	RCOperationErrorCodeOperationCancelled = 1000,
+	RCOperationErrorCodeTypeUndefined = 1001,
+	RCOperationErrorCodeIdentifierIsNull = 1002,
+	RCOperationErrorCodeJSONStatus = 1003
 };
