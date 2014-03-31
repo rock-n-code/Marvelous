a8073253a193470bee6686ae0844ff4f20f5f566
Javier Cicchelli <javier@rock-n-co.de>
2014-03-31 12:03:57 +0200

    Created the RCSummaryModel.

---
+++ Marvelous/RCSummaryModel.h
@@ -0,0 +1,11 @@
+//
+//  RCSummaryModel.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 31/03/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+@interface RCSummaryModel : NSObject
+
+@end
