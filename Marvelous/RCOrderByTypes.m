dd8085102aa06777ba5538bc1e01c775ef7f16c3
Javier Cicchelli <javier@rock-n-co.de>
2014-04-01 14:29:18 +0200

    Created and defined some constants on RCOrderByTypes.

---
+++ Marvelous/RCOrderByTypes.m
@@ -0,0 +1,14 @@
+//
+//  RCOrderByTypes.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 01/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCOrderByTypes.h"
+
+NSString * const RCOrderByTypeNameAscending = @"name";
+NSString * const RCOrderByTypeNameDescending = @"-name";
+NSString * const RCOrderByTypeDateModifiedAscending = @"modified";
+NSString * const RCOrderByTypeDateModifiedDescending = @"-modified	";
