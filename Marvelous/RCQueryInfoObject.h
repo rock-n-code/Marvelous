1bbd7bf3123ba624364e50d2cdff1d56136c8d64
Javier Cicchelli <javier@rock-n-co.de>
2014-04-01 12:51:29 +0200

    Created the RCQueryInfoObject.

---
+++ Marvelous/RCQueryInfoObject.h
@@ -0,0 +1,11 @@
+//
+//  RCQueryInfoObject.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 01/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+@interface RCQueryInfoObject : NSObject
+
+@end
---
+++ Marvelous/RCQueryInfoObject.m
@@ -0,0 +1,17 @@
+//
+//  RCQueryInfoObject.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 01/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCQueryInfoObject.h"
+
+@interface RCQueryInfoObject ()
+
+@end
+
+@implementation RCQueryInfoObject
+
+@end
