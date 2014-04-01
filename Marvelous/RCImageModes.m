104a7703ac7ca51476d96c6ef31f33b9bda2e6a6
Javier Cicchelli <javier@rock-n-co.de>
2014-04-02 01:15:15 +0200

    Created and defined the constants on RCImageModes.

---
+++ Marvelous/RCImageModes.m
@@ -0,0 +1,14 @@
+//
+//  RCImageModes.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 02/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCImageModes.h"
+
+NSString * const RCImageModeDetail = @"detail";
+NSString * const RCImageModeSquare = @"standard";
+NSString * const RCImageModePortrait = @"portrait";
+NSString * const RCImageModeLandscape = @"landscape";
