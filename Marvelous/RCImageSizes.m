013ba32e6bb53eeaa72e89470acfd8e9aac4a09b
Javier Cicchelli <javier@rock-n-co.de>
2014-04-02 01:18:36 +0200

    Created and defined the constants on RCImageSizes.

---
+++ Marvelous/RCImageSizes.m
@@ -0,0 +1,18 @@
+//
+//  RCImageSizes.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 02/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCImageSizes.h"
+
+NSString * const RCImageSizeSmall = @"small";
+NSString * const RCImageSizeMedium = @"medium";
+NSString * const RCImageSizeLarge = @"large";
+NSString * const RCImageSizeXLarge = @"xlarge";
+NSString * const RCImageSizeAmazing = @"amazing";
+NSString * const RCImageSizeFantastic = @"fantastic";
+NSString * const RCImageSizeUncanny = @"uncanny";
+NSString * const RCImageSizeIncredible = @"incredible";
