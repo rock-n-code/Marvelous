4d5225f5a28455a718353bd3b67e5488201361b5
Javier Cicchelli <javier@rock-n-co.de>
2014-04-01 14:02:37 +0200

    Created and defined the enumerations on RCFilterOrderByTypes.

---
+++ Marvelous/RCFilterOrderByTypes.h
@@ -0,0 +1,14 @@
+//
+//  RCFilterOrderByTypes.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 01/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+typedef NS_ENUM(NSInteger, RCFilterOrderByTypes) {
+	RCFilterOrderByTypeNameAscending,
+	RCFilterOrderByTypeNameDescending,
+	RCFilterOrderByTypeDateModifiedAscending,
+	RCFilterOrderByTypeDateModifiedDescending
+};
