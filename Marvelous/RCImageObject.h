450b823bb19e8b784a9bef225b9b014474d7eb93
Javier Cicchelli <javier@rock-n-co.de>
2014-04-01 22:33:59 +0200

    Created the RCImageObject.

---
+++ Marvelous/RCImageObject.h
@@ -0,0 +1,13 @@
+//
+//  RCImageObject.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 01/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCObject.h"
+
+@interface RCImageObject : RCObject
+
+@end
---
+++ Marvelous/RCImageObject.m
@@ -0,0 +1,13 @@
+//
+//  RCImageObject.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 01/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCImageObject.h"
+
+@implementation RCImageObject
+
+@end
