05590038f31840748fa5b2675a99c7910b3cba3e
Javier Cicchelli <javier@rock-n-co.de>
2014-04-02 12:22:10 +0200

    Created the RCEventObject.

---
+++ Marvelous/RCEventObject.h
@@ -0,0 +1,13 @@
+//
+//  RCEventObject.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 02/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCObject.h"
+
+@interface RCEventObject : RCObject
+
+@end
---
+++ Marvelous/RCEventObject.m
@@ -0,0 +1,17 @@
+//
+//  RCEventObject.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 02/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCEventObject.h"
+
+@interface RCEventObject ()
+
+@end
+
+@implementation RCEventObject
+
+@end
