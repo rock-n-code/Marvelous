3970e60086b67a47c0dfc8b2a1376eaa36790ae1
Javier Cicchelli <javier@rock-n-co.de>
2014-04-02 11:41:01 +0200

    Created the RCEventFilter.

---
+++ MarvelousTests/RCEventFilterTests.m
@@ -0,0 +1,34 @@
+//
+//  RCEventFilterTests.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 02/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RCEventFilter.h"
+
+@interface RCEventFilterTests : XCTestCase
+
+@end
+
+@implementation RCEventFilterTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
