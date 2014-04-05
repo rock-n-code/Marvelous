bd54d8d99b08f4dbcadc54e29f5789b9f58d8fac
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 00:29:48 +0200

    Created the RCStoryFilter.

---
+++ Marvelous/RCStoryFilter.m
@@ -0,0 +1,17 @@
+//
+//  RCStoryFilter.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCStoryFilter.h"
+
+@interface RCStoryFilter ()
+
+@end
+
+@implementation RCStoryFilter
+
+@end
---
+++ MarvelousTests/RCStoryFilterTests.m
@@ -0,0 +1,34 @@
+//
+//  RCStoryFilterTests.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RCStoryFilter.h"
+
+@interface RCStoryFilterTests : XCTestCase
+
+@end
+
+@implementation RCStoryFilterTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
