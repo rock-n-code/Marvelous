0c62ab2f8f7a57a819d3280c01eb1649623578d1
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 00:57:41 +0200

    Created the RCStoryObject.

---
+++ Marvelous/RCStoryObject.m
@@ -0,0 +1,17 @@
+//
+//  RCStoryObject.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCStoryObject.h"
+
+@interface RCStoryObject ()
+
+@end
+
+@implementation RCStoryObject
+
+@end
---
+++ MarvelousTests/RCStoryObjectTests.m
@@ -0,0 +1,34 @@
+//
+//  RCStoryObjectTests.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RCStoryObject.h"
+
+@interface RCStoryObjectTests : XCTestCase
+
+@end
+
+@implementation RCStoryObjectTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
