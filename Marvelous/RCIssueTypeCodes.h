6fdc3f54878ef188cbd4b564873a60bd3af95121
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 19:29:10 +0200

    Defined the enumerations and constants on RCIssueTypeCodes and RCIssueTypes.

---
+++ Marvelous/RCIssueTypeCodes.h
@@ -0,0 +1,13 @@
+//
+//  RCIssueTypeCodes.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+typedef NS_ENUM(NSInteger, RCIssueTypeCodes) {
+	RCIssueTypeCodeUndefined = 0,
+	RCIssueTypeCodeComic,
+	RCIssueTypeCodeCollection
+};
