cc74126bbcce542ee7b1be7a0716d975fa16158b
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 17:27:13 +0200

    Defined several readonly public properties on RCSeriesObject.

--- Marvelous/RCSeriesObject.m
+++ Marvelous/RCSeriesObject.m
@@ -10,6 +10,24 @@
 
 @interface RCSeriesObject ()
 
+@property (nonatomic, strong) NSNumber *identifier;
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *summary;
+@property (nonatomic, strong) NSURL *resourceURI;
+@property (nonatomic, strong) NSArray *urls;
+@property (nonatomic, strong) NSNumber *startYear;
+@property (nonatomic, strong) NSNumber *endYear;
+@property (nonatomic, strong) NSString *rating;
+@property (nonatomic, strong) NSDate *lastModified;
+@property (nonatomic, strong) RCImageObject *thumbnail;
+@property (nonatomic, strong) RCListObject *comics;
+@property (nonatomic, strong) RCListObject *stories;
+@property (nonatomic, strong) RCListObject *events;
+@property (nonatomic, strong) RCListObject *characters;
+@property (nonatomic, strong) RCListObject *creators;
+@property (nonatomic, strong) RCSummaryObject *previous;
+@property (nonatomic, strong) RCSummaryObject *next;
+
 @end
 
 @implementation RCSeriesObject
--- MarvelousTests/RCSeriesObjectTests.m
+++ MarvelousTests/RCSeriesObjectTests.m
@@ -26,9 +26,27 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testInit
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+	RCSeriesObject *series = [[RCSeriesObject alloc] init];
+
+	XCTAssertNil(series.identifier, @"\"%s\" is expecting the property 'identifier' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.title, @"\"%s\" is expecting the property 'name' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.summary, @"\"%s\" is expecting the property 'bio' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.resourceURI, @"\"%s\" is expecting the property 'resourceURI' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.urls, @"\"%s\" is expecting the property 'urls' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.startYear, @"\"%s\" is expecting the property 'startYear' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.endYear, @"\"%s\" is expecting the property 'endYear' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.rating, @"\"%s\" is expecting the property 'rating' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.lastModified, @"\"%s\" is expecting the property 'lastModified' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.thumbnail, @"\"%s\" is expecting the property 'thumbnail' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.comics, @"\"%s\" is expecting the property 'comics' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.stories, @"\"%s\" is expecting the property 'stories' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.events, @"\"%s\" is expecting the property 'events' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.characters, @"\"%s\" is expecting the property 'characters' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.creators, @"\"%s\" is expecting the property 'creators' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.previous, @"\"%s\" is expecting the property 'previous' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(series.next, @"\"%s\" is expecting the property 'next' to be NULL.", __PRETTY_FUNCTION__);
 }
 
 @end
