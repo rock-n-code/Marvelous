6820b8646bad34cc9d1b2e89d4245c13f1ed2475
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 12:49:12 +0200

    Created the RCCreatorObject.

---
+++ Marvelous/RCCreatorObject.m
@@ -0,0 +1,17 @@
+//
+//  RCCreatorObject.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCCreatorObject.h"
+
+@interface RCCreatorObject ()
+
+@end
+
+@implementation RCCreatorObject
+
+@end
