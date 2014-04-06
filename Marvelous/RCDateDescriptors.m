98e538df83e64ebb033766c7105aa7033be9ac2c
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 19:39:27 +0200

    Defined the enumerations and constants on RCDateDescriptorCodes and RCDateDescriptors.

---
+++ Marvelous/RCDateDescriptors.m
@@ -0,0 +1,14 @@
+//
+//  RCDateDescriptors.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCDateDescriptors.h"
+
+NSString * const RCDateDescriptorLastWeek = @"lastWeek";
+NSString * const RCDateDescriptorThisWeek = @"thisWeek";
+NSString * const RCDateDescriptorNextWeek = @"nextWeek";
+NSString * const RCDateDescriptorThisMonth = @"thisMonth";
