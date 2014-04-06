88c5279fef84246e546c79d65677a3713a0d8cf3
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 13:54:06 +0200

    Defined the enumerations and constants on RCFormatTypeCodes and RCFormatTypes.

---
+++ Marvelous/RCFormatTypes.h
@@ -0,0 +1,16 @@
+//
+//  RCFormatTypes.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+extern NSString * const RCFormatTypeComic;
+extern NSString * const RCFormatTypeMagazine;
+extern NSString * const RCFormatTypeTradePaperback;
+extern NSString * const RCFormatTypeHardcover;
+extern NSString * const RCFormatTypeDigest;
+extern NSString * const RCFormatTypeGraphicNovel;
+extern NSString * const RCFormatTypeDigitalComic;
+extern NSString * const RCFormatTypeInfiniteComic;
