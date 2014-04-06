be56c083b9fec3129a28c1b7bc3c700a3678a777
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 23:36:58 +0200

    Created the RCComicsObject.

---
+++ Marvelous/RCComicsObject.h
@@ -0,0 +1,13 @@
+//
+//  RCComicsObject.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCObject.h"
+
+@interface RCComicsObject : RCObject
+
+@end
---
+++ MarvelousTests/RCComicsObjectTests.m
@@ -0,0 +1,34 @@
+//
+//  RCComicsObjectTests.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RCComicsObject.h"
+
+@interface RCComicsObjectTests : XCTestCase
+
+@end
+
+@implementation RCComicsObjectTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
