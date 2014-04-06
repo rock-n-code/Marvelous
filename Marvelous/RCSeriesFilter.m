c4b22abcd686b306768afadb4b9383bd62c14937
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 17:10:16 +0200

    Added the 'stringfiedContains' and 'stringfiedSeriesType' private readonly properties on RCSeriesFilter and implemented its getter methods.

--- Marvelous/RCSeriesFilter.m
+++ Marvelous/RCSeriesFilter.m
@@ -7,8 +7,13 @@
 //
 
 #import "RCSeriesFilter.h"
+#import "RCFormatTypes.h"
+#import "RCSeriesTypes.h"
 
 @interface RCSeriesFilter ()
+
+@property (nonatomic, readonly, strong) NSString *stringfiedContains;
+@property (nonatomic, readonly, strong) NSString *stringfiedSeriesType;
 
 @end
 
@@ -27,6 +32,54 @@
 - (RCAPITypes)type
 {
 	return RCAPITypeSeries;
+}
+
+- (NSString *)stringfiedContains
+{
+	NSMutableArray *strings = [NSMutableArray array];
+
+	[self.contains enumerateObjectsUsingBlock:^(NSNumber *formatType, NSUInteger index, BOOL *stop) {
+		if (formatType.integerValue != RCFormatTypeCodeUndefined) {
+			NSInteger type = formatType.integerValue;
+			NSString *string = @"";
+
+			if (type == RCFormatTypeCodeComic) {
+				string = RCFormatTypeComic;
+			} else if (type == RCFormatTypeCodeMagazine) {
+				string = RCFormatTypeMagazine;
+			} else if (type == RCFormatTypeCodeTradePaperback) {
+				string = RCFormatTypeTradePaperback;
+			} else if (type == RCFormatTypeCodeHardcover) {
+				string = RCFormatTypeHardcover;
+			} else if (type == RCFormatTypeCodeDigest) {
+				string = RCFormatTypeDigest;
+			} else if (type == RCFormatTypeCodeGraphicNovel) {
+				string = RCFormatTypeGraphicNovel;
+			} else if (type == RCFormatTypeCodeDigitalComic) {
+				string = RCFormatTypeDigitalComic;
+			} else {
+				string = RCFormatTypeInfiniteComic;
+			}
+
+			[strings addObject:string];
+		}
+	}];
+
+	return [strings componentsJoinedByString:@","];
+}
+
+- (NSString *)stringfiedSeriesType
+{
+	switch (self.seriesType) {
+		case RCSeriesTypeCodeCollection:
+			return RCSeriesTypeCollection;
+		case RCSeriesTypeCodeOneShot:
+			return RCSeriesTypeOneShot;
+		case RCSeriesTypeCodeLimited:
+			return RCSeriesTypeLimited;
+		default:
+			return RCSeriesTypeOngoing;
+	}
 }
 
 @end
