3d8f1e1c69952f558e4ae2e2b13116e2fa9a8742
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 11:56:56 +0200

    Defined the public properties on RCCreatorFilter.

--- Marvelous/RCCreatorFilter.h
+++ Marvelous/RCCreatorFilter.h
@@ -10,4 +10,17 @@
 
 @interface RCCreatorFilter : RCFilter
 
+@property (nonatomic, strong) NSString *firstName;
+@property (nonatomic, strong) NSString *middleName;
+@property (nonatomic, strong) NSString *lastName;
+@property (nonatomic, strong) NSString *suffix;
+@property (nonatomic, strong) NSString *nameStartsWith;
+@property (nonatomic, strong) NSString *firstNameStartsWith;
+@property (nonatomic, strong) NSString *middleNameStartsWith;
+@property (nonatomic, strong) NSString *lastNameStartsWith;
+@property (nonatomic, strong) NSArray *comics;
+@property (nonatomic, strong) NSArray *series;
+@property (nonatomic, strong) NSArray *events;
+@property (nonatomic, strong) NSArray *stories;
+
 @end
