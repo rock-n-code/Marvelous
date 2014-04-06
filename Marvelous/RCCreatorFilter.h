6c572c363450ccd20a46a189d5ac69c403a4992b
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 11:53:43 +0200

    Created the RCCreatorFilter.

---
+++ Marvelous/RCCreatorFilter.h
@@ -0,0 +1,13 @@
+//
+//  RCCreatorFilter.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCFilter.h"
+
+@interface RCCreatorFilter : RCFilter
+
+@end
---
+++ MarvelousTests/RCCreatorFilterTests.m
@@ -0,0 +1,34 @@
+//
+//  RCCreatorFilterTests.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RCCreatorFilter.h"
+
+@interface RCCreatorFilterTests : XCTestCase
+
+@end
+
+@implementation RCCreatorFilterTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
