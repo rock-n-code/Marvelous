182e1b31fd3f5190da5d3a8fe676aeca27835e0e
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 13:36:19 +0200

    Created the RCSeriesFilter.

---
+++ Marvelous/RCSeriesFilter.h
@@ -0,0 +1,13 @@
+//
+//  RCSeriesFilter.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCFilter.h"
+
+@interface RCSeriesFilter : RCFilter
+
+@end
---
+++ Marvelous/RCSeriesFilter.m
@@ -0,0 +1,17 @@
+//
+//  RCSeriesFilter.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCSeriesFilter.h"
+
+@interface RCSeriesFilter ()
+
+@end
+
+@implementation RCSeriesFilter
+
+@end
