5cb0ead44574625d36c33ed6596fb1064980ea67
Javier Cicchelli <javier@rock-n-co.de>
2014-04-07 00:10:13 +0200

    Created the RCTextObject.

---
+++ Marvelous/RCTextObject.h
@@ -0,0 +1,13 @@
+//
+//  RCTextObject.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 07/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCObject.h"
+
+@interface RCTextObject : RCObject
+
+@end
---
+++ Marvelous/RCTextObject.m
@@ -0,0 +1,17 @@
+//
+//  RCTextObject.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 07/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCTextObject.h"
+
+@interface RCTextObject ()
+
+@end
+
+@implementation RCTextObject
+
+@end
