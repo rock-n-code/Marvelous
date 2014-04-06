53fa4908440ef0540f993bf101fff4ebc8dfb40c
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 19:34:05 +0200

    Defined the enumerations and constants on RCBooleanValueCodes and RCBooleanValues.

---
+++ Marvelous/RCBooleanValueCodes.h
@@ -0,0 +1,13 @@
+//
+//  RCBooleanValueCodes.h
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+typedef NS_ENUM(NSInteger, RCBooleanValueCodes) {
+	RCBooleanValueCodeUndefined = 0,
+	RCBooleanValueCodeTrue,
+	RCBooleanValueCodeFalse
+};
