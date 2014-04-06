a72ca25cbd4efd8f01709032d71b80c19e58e31c
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 23:53:56 +0200

    Defined several public properties on RCComicsObject.

--- Marvelous/RCComicsObject.m
+++ Marvelous/RCComicsObject.m
@@ -10,6 +10,36 @@
 
 @interface RCComicsObject ()
 
+@property (nonatomic, strong) NSNumber *identifier;
+@property (nonatomic, strong) NSNumber *digitalIdentifier;
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSNumber *issueNumber;
+@property (nonatomic, strong) NSString *summary;
+@property (nonatomic, strong) NSString *variantDescription;
+@property (nonatomic, strong) NSDate *lastModified;
+@property (nonatomic, strong) NSString *isbn;
+@property (nonatomic, strong) NSString *upc;
+@property (nonatomic, strong) NSString *diamondCode;
+@property (nonatomic, strong) NSString *ean;
+@property (nonatomic, strong) NSString *issn;
+@property (nonatomic, strong) NSString *format;
+@property (nonatomic, strong) NSNumber *pageCount;
+@property (nonatomic, strong) NSArray *textObjects;
+@property (nonatomic, strong) NSURL *resourceURI;
+@property (nonatomic, strong) NSArray *urls;
+@property (nonatomic, strong) RCSummaryObject *series;
+@property (nonatomic, strong) NSArray *variants;
+@property (nonatomic, strong) NSArray *collections;
+@property (nonatomic, strong) NSArray *collectedIssues;
+@property (nonatomic, strong) NSArray *dates;
+@property (nonatomic, strong) NSArray *prices;
+@property (nonatomic, strong) RCImageObject *thumbnail;
+@property (nonatomic, strong) NSArray *images;
+@property (nonatomic, strong) RCListObject *creators;
+@property (nonatomic, strong) RCListObject *characters;
+@property (nonatomic, strong) RCListObject *stories;
+@property (nonatomic, strong) RCListObject *events;
+
 @end
 
 @implementation RCComicsObject
--- MarvelousTests/RCComicsObjectTests.m
+++ MarvelousTests/RCComicsObjectTests.m
@@ -26,9 +26,39 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testInit
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+	RCComicsObject *comics = [[RCComicsObject alloc] init];
+
+	XCTAssertNil(comics.identifier, @"\"%s\" is expecting the property 'identifier' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.digitalIdentifier, @"\"%s\" is expecting the property 'digitalIdentifier' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.title, @"\"%s\" is expecting the property 'title' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.issueNumber, @"\"%s\" is expecting the property 'issueNumber' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.summary, @"\"%s\" is expecting the property 'summary' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.variantDescription, @"\"%s\" is expecting the property 'variantDescription' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.lastModified, @"\"%s\" is expecting the property 'lastModified' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.isbn, @"\"%s\" is expecting the property 'isbn' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.upc, @"\"%s\" is expecting the property 'upc' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.diamondCode, @"\"%s\" is expecting the property 'diamondCode' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.ean, @"\"%s\" is expecting the property 'ean' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.issn, @"\"%s\" is expecting the property 'issn' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.format, @"\"%s\" is expecting the property 'format' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.pageCount, @"\"%s\" is expecting the property 'pageCount' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.textObjects, @"\"%s\" is expecting the property 'textObjects' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.resourceURI, @"\"%s\" is expecting the property 'resourceURI' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.urls, @"\"%s\" is expecting the property 'urls' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.series, @"\"%s\" is expecting the property 'series' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.variants, @"\"%s\" is expecting the property 'variants' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.collections, @"\"%s\" is expecting the property 'collections' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.collectedIssues, @"\"%s\" is expecting the property 'collectedIssues' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.dates, @"\"%s\" is expecting the property 'dates' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.prices, @"\"%s\" is expecting the property 'prices' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.thumbnail, @"\"%s\" is expecting the property 'thumbnail' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.images, @"\"%s\" is expecting the property 'images' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.creators, @"\"%s\" is expecting the property 'creators' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.characters, @"\"%s\" is expecting the property 'characters' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.stories, @"\"%s\" is expecting the property 'stories' to be NULL.", __PRETTY_FUNCTION__);
+	XCTAssertNil(comics.events, @"\"%s\" is expecting the property 'events' to be NULL.", __PRETTY_FUNCTION__);
 }
 
 @end
