05a77ef0d50e4dae7735db1d86ba4f461bde07db
Javier Cicchelli <javier@rock-n-co.de>
2014-04-06 17:15:01 +0200

    Created the RCSeriesObject.

---
+++ Marvelous/RCSeriesObject.m
@@ -0,0 +1,17 @@
+//
+//  RCSeriesObject.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "RCSeriesObject.h"
+
+@interface RCSeriesObject ()
+
+@end
+
+@implementation RCSeriesObject
+
+@end
---
+++ MarvelousTests/RCSeriesObjectTests.m
@@ -0,0 +1,34 @@
+//
+//  RCSeriesObjectTests.m
+//  Marvelous
+//
+//  Created by Javier Cicchelli on 06/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RCSeriesObject.h"
+
+@interface RCSeriesObjectTests : XCTestCase
+
+@end
+
+@implementation RCSeriesObjectTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
