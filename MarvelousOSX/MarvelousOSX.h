3aa898eeb4a15e5ed7e8b62b1d8c910bc5f56b34
Javier Cicchelli <javier@rock-n-co.de>
2014-04-09 01:24:09 +0200

    Added the OSX and its respective test targets to the project.

---
+++ MarvelousOSX/MarvelousOSX.h
@@ -0,0 +1,13 @@
+//
+//  MarvelousOSX.h
+//  MarvelousOSX
+//
+//  Created by Javier Cicchelli on 09/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MarvelousOSX : NSObject
+
+@end
---
+++ MarvelousOSX/MarvelousOSX.m
@@ -0,0 +1,13 @@
+//
+//  MarvelousOSX.m
+//  MarvelousOSX
+//
+//  Created by Javier Cicchelli on 09/04/14.
+//  Copyright (c) 2014 Rock & Code. All rights reserved.
+//
+
+#import "MarvelousOSX.h"
+
+@implementation MarvelousOSX
+
+@end
