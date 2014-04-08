53d9af175c703053667427f4f5346b85ed29e43a
Javier Cicchelli <javier@rock-n-co.de>
2014-04-08 21:26:21 +0200

    Imported the headers of the filters, objects and enumerations that should been public on the Marvelous header file.

--- Marvelous/Marvelous.h
+++ Marvelous/Marvelous.h
@@ -8,3 +8,37 @@
 
 // API Interface
 #import <Marvelous/RCMarvelAPI.h>
+
+// Filters
+#import <Marvelous/RCCharacterFilter.h>
+#import <Marvelous/RCComicsFilter.h>
+#import <Marvelous/RCCreatorFilter.h>
+#import <Marvelous/RCEventFilter.h>
+#import <Marvelous/RCSeriesFilter.h>
+#import <Marvelous/RCStoryFilter.h>
+
+// Objects
+#import <Marvelous/RCCharacterObject.h>
+#import <Marvelous/RCComicsObject.h>
+#import <Marvelous/RCCreatorObject.h>
+#import <Marvelous/RCEventObject.h>
+#import <Marvelous/RCSeriesObject.h>
+#import <Marvelous/RCStoryObject.h>
+#import <Marvelous/RCListObject.h>
+#import <Marvelous/RCImageObject.h>
+#import <Marvelous/RCComicPriceObject.h>
+#import <Marvelous/RCComicDateObject.h>
+#import <Marvelous/RCTextObject.h>
+#import <Marvelous/RCSummaryObject.h>
+#import <Marvelous/RCURLObject.h>
+
+// Enumerations
+#import <Marvelous/RCAPITypes.h>
+#import <Marvelous/RCOrderByTypeCodes.h>
+#import <Marvelous/RCFormatTypeCodes.h>
+#import <Marvelous/RCSeriesTypeCodes.h>
+#import <Marvelous/RCIssueTypeCodes.h>
+#import <Marvelous/RCDateDescriptorCodes.h>
+#import <Marvelous/RCBooleanValueCodes.h>
+#import <Marvelous/RCImageModeCodes.h>
+#import <Marvelous/RCImageSizeCodes.h>
