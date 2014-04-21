af28812088b154077384603364d0d98b696f25c8
Javier Cicchelli <javier@rock-n-co.de>
2014-04-21 10:44:58 +0200

    Documented RCResponseKeys.

--- Marvelous/RCResponseKeys.h
+++ Marvelous/RCResponseKeys.h
@@ -23,73 +23,504 @@
 //  THE SOFTWARE.
 //
 
+/*!
+ @header
+
+ RCResponseKeys constants.
+
+ @author Javier Cicchelli (\@monsieur_rock)
+
+ @copyright 2014, Rock & Code released under MIT License. See the LICENSE file for more information.
+
+ @version 0.6.2
+ */
+
+/*!
+ @constant
+
+ "Code" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyCode;
+
+/*!
+ @constant
+
+ "Message" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyMessage;
+
+/*!
+ @constant
+
+ "Status" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyStatus;
+
+/*!
+ @constant
+
+ "Copyright" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyCopyright;
+
+/*!
+ @constant
+
+ "Attribution Text" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyAttributionText;
+
+/*!
+ @constant
+
+ "Attribution HTML" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyAttributionHTML;
+
+/*!
+ @constant
+
+ "Data" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyData;
+
+/*!
+ @constant
+
+ "ETag" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyETag;
+
+/*!
+ @constant
+
+ "Offset" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyOffset;
+
+/*!
+ @constant
+
+ "Limit" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyLimit;
+
+/*!
+ @constant
+
+ "Total" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyTotal;
+
+/*!
+ @constant
+
+ "Count" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyCount;
+
+/*!
+ @constant
+
+ "Results" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyResults;
+
+/*!
+ @constant
+
+ "Identifier" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyIdentifier;
+
+/*!
+ @constant
+
+ "Digital Identifier" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyDigitalIdentifier;
+
+/*!
+ @constant
+
+ "First Name" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyFirstName;
+
+/*!
+ @constant
+
+ "Middle Name" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyMiddleName;
+
+/*!
+ @constant
+
+ "Last Name" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyLastName;
+
+/*!
+ @constant
+
+ "Suffix" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeySuffix;
+
+/*!
+ @constant
+
+ "Name" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyName;
+
+/*!
+ @constant
+
+ "Full Name" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyFullName;
+
+/*!
+ @constant
+
+ "Title" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyTitle;
+
+/*!
+ @constant
+
+ "Issue Number" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyIssueNumber;
+
+/*!
+ @constant
+
+ "Description" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyDescription;
+
+/*!
+ @constant
+
+ "Variant Description" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyVariantDescription;
+
+/*!
+ @constant
+
+ "Start" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyStart;
+
+/*!
+ @constant
+
+ "End" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyEnd;
+
+/*!
+ @constant
+
+ "Modified" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyModified;
+
+/*!
+ @constant
+
+ "ISBN" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyISBN;
+
+/*!
+ @constant
+
+ "UPC" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyUPC;
+
+/*!
+ @constant
+
+ "Diamond Code" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyDiamondCode;
+
+/*!
+ @constant
+
+ "EAN" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyEAN;
+
+/*!
+ @constant
+
+ "ISSN" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyISSN;
+
+/*!
+ @constant
+
+ "Format" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyFormat;
+
+/*!
+ @constant
+
+ "Page Count" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyPageCount;
+
+/*!
+ @constant
+
+ "Text Objects" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyTextObjects;
+
+/*!
+ @constant
+
+ "Language" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyLanguage;
+
+/*!
+ @constant
+
+ "Text" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyText;
+
+/*!
+ @constant
+
+ "Resource URI" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyResourceURI;
+
+/*!
+ @constant
+
+ "URLs" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyURLs;
+
+/*!
+ @constant
+
+ "Start Year" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyStartYear;
+
+/*!
+ @constant
+
+ "End Year" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyEndYear;
+
+/*!
+ @constant
+
+ "Rating" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyRating;
+
+/*!
+ @constant
+
+ "Thumbnail" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyThumbnail;
+
+/*!
+ @constant
+
+ "Images" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyImages;
+
+/*!
+ @constant
+
+ "Comics" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyComics;
+
+/*!
+ @constant
+
+ "Stories" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyStories;
+
+/*!
+ @constant
+
+ "Events" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyEvents;
+
+/*!
+ @constant
+
+ "Series" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeySeries;
+
+/*!
+ @constant
+
+ "Variants" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyVariants;
+
+/*!
+ @constant
+
+ "Collections" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyCollections;
+
+/*!
+ @constant
+
+ "Collected Issues" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyCollectedIssues;
+
+/*!
+ @constant
+
+ "Dates" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyDates;
+
+/*!
+ @constant
+
+ "Prices" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyPrices;
+
+/*!
+ @constant
+
+ "Creators" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyCreators;
+
+/*!
+ @constant
+
+ "Characters" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyCharacters;
+
+/*!
+ @constant
+
+ "Original Issue" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyOriginalIssue;
+
+/*!
+ @constant
+
+ "Previous" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyPrevious;
+
+/*!
+ @constant
+
+ "Next" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyNext;
+
+/*!
+ @constant
+
+ "Type" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyType;
+
+/*!
+ @constant
+
+ "URL" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyURL;
+
+/*!
+ @constant
+
+ "Path" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyPath;
+
+/*!
+ @constant
+
+ "Extension" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyExtension;
+
+/*!
+ @constant
+
+ "Date" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyDate;
+
+/*!
+ @constant
+
+ "Price" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyPrice;
+
+/*!
+ @constant
+
+ "Role" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyRole;
+
+/*!
+ @constant
+
+ "Available" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyAvailable;
+
+/*!
+ @constant
+
+ "Returned" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyReturned;
+
+/*!
+ @constant
+
+ "Collection URI" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyCollectionURI;
+
+/*!
+ @constant
+
+ "Items" field to be used as a key on a JSON dictionary returned by an API response.
+ */
 extern NSString * const RCResponseKeyItems;
