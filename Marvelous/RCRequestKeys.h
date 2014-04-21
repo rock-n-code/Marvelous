f8546f39d3557a71689ae04aad0951156454385e
Javier Cicchelli <javier@rock-n-co.de>
2014-04-21 10:36:22 +0200

    Documented RCRequestKeys.

--- Marvelous/RCRequestKeys.h
+++ Marvelous/RCRequestKeys.h
@@ -23,45 +23,308 @@
 //  THE SOFTWARE.
 //
 
+/*!
+ @header
+
+ RCRequestKeys constants.
+
+ @author Javier Cicchelli (\@monsieur_rock)
+
+ @copyright 2014, Rock & Code released under MIT License. See the LICENSE file for more information.
+
+ @version 0.6.2
+ */
+
+/*!
+ @constant
+
+ "Timestamp" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyTimestamp;
+
+/*!
+ @constant
+
+ "API Key" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyAPIKey;
+
+/*!
+ @constant
+
+ "Hash" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyHash;
+
+/*!
+ @constant
+
+ "Identifier" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyIdentifier;
+
+/*!
+ @constant
+
+ "First Name" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyFirstName;
+
+/*!
+ @constant
+
+ "Middle Name" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyMiddleName;
+
+/*!
+ @constant
+
+ "Last Name" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyLastName;
+
+/*!
+ @constant
+
+ "Suffix" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeySuffix;
+
+/*!
+ @constant
+
+ "Name" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyName;
+
+/*!
+ @constant
+
+ "Name Starts With" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyNameStartsWith;
+
+/*!
+ @constant
+
+ "First Name Starts With" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyFirstNameStartsWith;
+
+/*!
+ @constant
+
+ "Middle Name Starts With" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyMiddleNameStartsWith;
+
+/*!
+ @constant
+
+ "Last Name Starts With" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyLastNameStartsWith;
+
+/*!
+ @constant
+
+ "Title" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyTitle;
+
+/*!
+ @constant
+
+ "Title Starts With" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyTitleStartsWith;
+
+/*!
+ @constant
+
+ "Format" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyFormat;
+
+/*!
+ @constant
+
+ "Format Type" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyFormatType;
+
+/*!
+ @constant
+
+ "No Variants" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyNoVariants;
+
+/*!
+ @constant
+
+ "Start Year" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyStartYear;
+
+/*!
+ @constant
+
+ "Date Descriptor" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyDateDescriptor;
+
+/*!
+ @constant
+
+ "Date Range" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyDateRange;
+
+/*!
+ @constant
+
+ "Diamond Code" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyDiamondCode;
+
+/*!
+ @constant
+
+ "Digital Identifier" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyDigitalIdentifier;
+
+/*!
+ @constant
+
+ "UPC" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyUPC;
+
+/*!
+ @constant
+
+ "ISBN" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyISBN;
+
+/*!
+ @constant
+
+ "EAN" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyEAN;
+
+/*!
+ @constant
+
+ "ISSN" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyISSN;
+
+/*!
+ @constant
+
+ "Has Digital Issue" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyHasDigitalIssue;
+
+/*!
+ @constant
+
+ "Modified Since" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyModifiedSince;
+
+/*!
+ @constant
+
+ "Creators" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyCreators;
+
+/*!
+ @constant
+
+ "Characters" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyCharacters;
+
+/*!
+ @constant
+
+ "Comics" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyComics;
+
+/*!
+ @constant
+
+ "Series" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeySeries;
+
+/*!
+ @constant
+
+ "Events" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyEvents;
+
+/*!
+ @constant
+
+ "Stories" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyStories;
+
+/*!
+ @constant
+
+ "Shared Appearances" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeySharedAppearances;
+
+/*!
+ @constant
+
+ "Collaborators" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyCollaborators;
+
+/*!
+ @constant
+
+ "Series Type" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeySeriesType;
+
+/*!
+ @constant
+
+ "Contains" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyContains;
+
+/*!
+ @constant
+
+ "Order By" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyOrderBy;
+
+/*!
+ @constant
+
+ "Limit" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyLimit;
+
+/*!
+ @constant
+
+ "Offset" field to be used as a parameter key on an API request.
+ */
 extern NSString * const RCRequestKeyOffset;
